6c9a95eb71abeaa8df0be05a486658499ef68612
Steven Bainter <bainter92x@gmail.com>
2020-05-15 14:47:46 -0400

    Fixed EmailService

--- EmailService/Program.cs
+++ EmailService/Program.cs
@@ -16,8 +16,8 @@ namespace EmailService
         {
             try
             {
-
-                List<String> emails = new List<String>();
+                List<string> ids = new List<string>();
+                List<string> emails = new List<string>();
 
                 var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MMMTestDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
@@ -25,7 +25,7 @@ namespace EmailService
                 {
                     //string parameter = "";
 
-                    string query = @"SELECT * FROM [MyMedsManager].[dbo].[AspNetUsers]";
+                    string query = @"SELECT * FROM [MMMTestDB].[dbo].[AspNetUsers]";
                     //string where = " WHERE parameter = @ParameterName"
                     SqlCommand command = new SqlCommand(query, connection);
                     //command.Parameters.AddWithValue("@ParameterName", parameter);
@@ -36,7 +36,9 @@ namespace EmailService
                     {
                         while (reader.Read())
                         {
+                            ids.Add(reader["Id"].ToString());
                             emails.Add(reader["NormalizedEmail"].ToString());
+                            
                         }
 
                         connection.Close();
@@ -44,9 +46,11 @@ namespace EmailService
                 };
 
 
-                foreach (string email in emails)
+                for (int i = 0; i < ids.Count; i++)
                 {
-                    SendEmail(email);
+                    string id = ids[i];
+                    string email = emails[i];
+                    SendEmail(id, email);
                 }
 
             }
@@ -62,7 +66,7 @@ namespace EmailService
             }
         }
 
-        public static void SendEmail(string toAddress)
+        public static void SendEmail(string id, string email)
         {
 
             SmtpClient mySmtpClient = new SmtpClient("smtp.gmail.com");
@@ -70,12 +74,12 @@ namespace EmailService
             // set smtp-client with basicAuthentication
             mySmtpClient.UseDefaultCredentials = false;
             System.Net.NetworkCredential basicAuthenticationInfo = new
-               System.Net.NetworkCredential("bainter92x@gmail.com", "password");
+               System.Net.NetworkCredential("bainter92x@gmail.com", "X29Retniab!@#$");
             mySmtpClient.Credentials = basicAuthenticationInfo;
             mySmtpClient.EnableSsl = true;
             // add from,to mailaddresses
             MailAddress from = new MailAddress("bainter92x@gmail.com");
-            MailAddress to = new MailAddress(toAddress);
+            MailAddress to = new MailAddress(email);
             MailMessage myMail = new System.Net.Mail.MailMessage(from, to);
 
             // add ReplyTo
@@ -83,14 +87,52 @@ namespace EmailService
             myMail.ReplyToList.Add(replyto);
 
             // set subject and encoding
-            myMail.Subject = "Test message";
+            myMail.Subject = "MyMedsManager";
             myMail.SubjectEncoding = System.Text.Encoding.UTF8;
 
             // set body-message and encoding
-            myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+            List<string> userIds = new List<string>();
+            List<string> medicines = new List<string>();
+            List<String> medQuantity = new List<String>();
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MMMTestDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //string parameter = "";
+
+                string query = @"SELECT * FROM [MMMTestDB].[dbo].[Medication]";
+                //string where = " WHERE parameter = @ParameterName"
+                SqlCommand command = new SqlCommand(query, connection);
+                //command.Parameters.AddWithValue("@ParameterName", parameter);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        userIds.Add(reader["UserId"].ToString());
+                        medicines.Add(reader["Medicine"].ToString());
+                        medQuantity.Add(reader["MedQuantity"].ToString());
+                    }
+
+                    connection.Close();
+                }
+            };
+
+            for (int i = 0; i < userIds.Count; i++)
+            {
+                if (userIds[i] == id)
+                {
+                    string medicine = medicines[i];
+                    string medQty = medQuantity[i];
+                    myMail.Body += $"You have {medQty} {medicine} left. ";
+                }
+            }
+
+            //myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
             myMail.BodyEncoding = System.Text.Encoding.UTF8;
             // text or html
-            myMail.IsBodyHtml = true;
+            myMail.IsBodyHtml = false;
 
             mySmtpClient.Send(myMail);
         }
